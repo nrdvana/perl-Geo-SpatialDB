0830425c97572a5d2201a5c00ffc0c340bf241ef
Michael Conrad <mike@nrdvana.net>
2016-06-12 08:53:04 -0400

    Bug fixes and test cases for LMDB_Storable

--- lib/Geo/SpatialDB/Storage/LMDB_Storable.pm
+++ lib/Geo/SpatialDB/Storage/LMDB_Storable.pm
@@ -1,16 +1,49 @@
 package Geo::SpatialDB::Storage::LMDB_Storable;
 use Moo 2;
-use LMDB_File ':flags';
+use LMDB_File ':flags', ':cursor_op', ':error';
 use Storable 'freeze', 'thaw';
 sub _croak { require Carp; goto &Carp::croak }
 use namespace::clean;
 
 extends 'Geo::SpatialDB::Storage';
 
+=head1 DESCRIPTION
+
+Storage engine LMDB_Storable uses (as the name implies) L<Storable> to serialize
+perl data structures, and LMDB (via the L<LMDB_File> module) for disk storage.
+I *think* this is the fastest possible way to persist Perl objects, but if you
+find something faster, I'd love to hear about it.
+
+=head1 ATTRIBUTES
+
+=head2 path
+
+The path for the LMDB files.  If this is an existing directory, LMDB will be
+initialized in the standard manner.  But if it is a file (or missing) then we
+treat it as the database file itself, which means the directory must be
+writable in order for LMDB to create the lock file along side of it.
+
+=head2 readonly
+
+Boolean.  If true, then open the DB in readonly mode.  Useful for read-only
+filesystems.
+
+=head2 mapsize
+
+The maximum size of the database; default is 3GB.  (LMDB needs this parameter)
+
+=head2 run_with_scissors
+
+If set to 1, then enable unsafe behavior in order to get some extra speed.
+Useful for large batch operations where the database can be thrown away if
+the operation fails catastrophically.
+
+=cut
+
 has path      => ( is => 'ro', required => 1 );
 has readonly  => ( is => 'ro', default => sub { 0 } );
 has mapsize   => ( is => 'ro', default => sub { 0xC0000000 } );
-has go_faster => ( is => 'ro', default => sub { 0 } );
+has run_with_scissors => ( is => 'ro', default => sub { 0 } );
 
 sub BUILD {
 	my $self= shift;
@@ -28,7 +61,7 @@ sub _build__env {
 		flags   =>
 			(-d $path? 0 : MDB_NOSUBDIR)
 			| ($self->readonly? MDB_RDONLY : 0)
-			| ($self->go_faster? MDB_WRITEMAP|MDB_NOMETASYNC : 0)
+			| ($self->run_with_scissors? MDB_WRITEMAP|MDB_NOMETASYNC : 0)
 		}
 	);
 }
@@ -48,25 +81,79 @@ sub die_invalid_assumption {
 }
 $storable_magic =~ /[\0-\x19]/ or die_invalid_assumption();
 
+=head1 METHODS
+
+=head2 get
+
+  my $value= $stor->get( $key );
+
+Get the value of a key, or undef if the key doesn't exist.
+
+=cut
+
 sub get {
 	my $v= shift->_db->get(shift);
-	$v= thaw($v) if substr($v, 0, 1) eq $storable_magic;
+	$v= thaw($v) if defined $v and substr($v, 0, 1) eq $storable_magic;
+	return $v;
 }
 
+=head2 put
+
+  $stor->put( $key, $value );
+
+Store a value in the database.  If the key exists it will overwrite the old value.
+If C<$value> is undefined, this deletes the key from the database.
+
+=cut
+
 sub put {
-	my $v= $_[2];
-	if (ref $v) {
+	my ($self, $k, $v)= @_;
+	if (!defined $v) {
+		return $self->_db->del($k);
+	}
+	elsif (ref $v) {
 		$v= freeze($v);
 		substr($v, 0, 1) eq $storable_magic
 			or die_invalid_assumption();
 	} else {
 		ord(substr($v, 0, 1)) > 0x1F or _croak("scalars must not start with control characters");
 	}
-	shift->_db->put(shift, $v);
+	$self->_db->put($k, $v);
 }
 
-sub delete {
-	shift->_db->del(shift);
+=head2 iterator
+
+  my $i= $stor->iterator;
+  # or
+  my $i= $stor->iterator( $from_key );
+  # then...
+  while (my $k= $i->()) { ... }
+  # or
+  while (my ($k, $v)= $i->()) { ... }
+
+Return a coderef which can iterate keys or key,value pairs.
+If you specify C<$from_key>, then iteration begins at the first key equal or
+greater to it.
+
+=cut
+
+sub iterator {
+	my ($self, $key)= @_;
+	my $op= defined $key? MDB_SET_RANGE : MDB_FIRST;
+	my $cursor= $self->_db->Cursor;
+	my $data;
+	return sub {
+		local $LMDB_File::die_on_err= 0;
+		my $ret= $cursor->get($key, $data, $op);
+		$op= MDB_NEXT;
+		if ($ret) {
+			return if $ret == MDB_NOTFOUND;
+			die $LMDB_File::last_err
+		}
+		return $key unless wantarray;
+		$data= thaw($data) if substr($data, 0, 1) eq $storable_magic;
+		return ($key, $data);
+	}
 }
 
 1;
---
+++ t/010-storage-lmdb.t
@@ -0,0 +1,57 @@
+use strict;
+use warnings;
+use Test::More;
+use FindBin;
+use File::Spec::Functions;
+use File::Path 'remove_tree','make_path';
+
+use_ok 'Geo::SpatialDB::Storage::LMDB_Storable' or die;
+
+my $tmpdir= catdir($FindBin::RealBin, 'tmp', $FindBin::Script);
+remove_tree($tmpdir, { error => \my $ignored });
+make_path($tmpdir or die "Can't create $tmpdir");
+
+my $store= Geo::SpatialDB::Storage::LMDB_Storable->new(
+	path => $tmpdir
+);
+
+$store->put(a => 1);
+$store->put(c => \3);
+$store->put(d => [1, 2, 3, 4]);
+$store->put(e => { a => 1, b => 2 });
+
+is( $store->get('a'), 1,                         'plain scalar' );
+is( $store->get('b'), undef,                     'missing key' );
+is_deeply( $store->get('c'), \3,                 'scalar ref' );
+is_deeply( $store->get('d'), [1, 2, 3, 4],       'array' );
+is_deeply( $store->get('e'), { a => 1, b => 2 }, 'hash' );
+
+my @keys;
+my $i= $store->iterator;
+while (my $k= $i->()) {
+	push @keys, $k;
+}
+is_deeply( \@keys, ['a', 'c', 'd', 'e'], 'iterate keys' );
+
+@keys= ();
+$i= $store->iterator('b');
+while (my $k= $i->()) {
+	push @keys, $k;
+}
+is_deeply( \@keys, ['c', 'd', 'e'], 'iterate keys from "b" onward' );
+
+@keys= ();
+my @vals;
+$i= $store->iterator('b');
+while (my ($k,$v)= $i->()) {
+	push @keys, $k;
+	push @vals, $v;
+}
+
+is_deeply( \@keys, ['c', 'd', 'e'], 'iterate (k,v) keys from "b" onward' );
+is_deeply( \@vals, [ \3, [1, 2, 3, 4], { a=>1, b=>2 } ], 'iterate (k,v) vals from "b" onward' );
+
+$store->put('d', undef);
+is( $store->get('d'), undef, 'deleted "d"' );
+
+done_testing;
