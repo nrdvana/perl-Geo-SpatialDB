9b6a2a06bdf40fb8355a143460c8a84b4fea4de1
Michael Conrad <mike@nrdvana.net>
2019-01-05 03:59:47 -0500

    Implement sort_vectors_by_heading

--- lib/Geo/SpatialDB/Export/MapPolygon3D/Vector.pm
+++ lib/Geo/SpatialDB/Export/MapPolygon3D/Vector.pm
@@ -6,9 +6,10 @@ use Exporter 'import';
 our @EXPORT_OK= qw( vector );
 
 sub new {
-	my $class= shift;
-	@_ >= 3 or croak "Require at least x,y,z arguments to Vector->new";
-	bless [ @_ ], ref($class)||$class;
+	my ($class, @vec)= @_;
+	@vec >= 2 or croak "Require at least x,y arguments to Vector->new";
+	$vec[2] ||= 0;
+	bless \@vec, ref($class)||$class;
 }
 sub vector {
 	__PACKAGE__->new(@_);
@@ -60,9 +61,9 @@ sub mag {
 }
 
 sub normalize {
-	my $scale= sqrt($_[0][0] * $_[0][0] + $_[0][1] * $_[0][1] + $_[0][2] * $_[0][2]);
-	if ($scale) {
-		$scale= 1/$scale;
+	my $mag_sq= $_[0][0] * $_[0][0] + $_[0][1] * $_[0][1] + $_[0][2] * $_[0][2];
+	if ($mag_sq && $mag_sq != 1) {
+		my $scale= 1/sqrt($mag_sq);
 		$_[0][0] *= $scale;
 		$_[0][1] *= $scale;
 		$_[0][2] *= $scale;
@@ -78,18 +79,45 @@ sub cross {
 	], ref $_[0];
 }
 
-# For vectors arrayed around an "up" vector, sort $a and $b in the order they would
-# occur counter clockwise from $self.  $self, $a, and $b must be unit vectors.
-sub angle_cmp {
-	my ($angle0, $angle90, $a, $b)= @_;
-	my $a_proj_0=  $a->dot($angle0);
-	my $a_proj_90= $a->dot($angle90);
-	my $b_proj_0=  $b->dot($angle0);
-	my $b_proj_90= $b->dot($angle90);
-	return
-		($a_proj_90 >= 0? 1-$a_proj_self : 3+$a_proj_self)
-		<=>
-		($b_proj_90 >= 0? 1-$b_proj_self : 3+$b_proj_self);
+=head2 sort_cclockwise
+
+Sort a list of vectors in order of counter-clockwise "heading" relative to this vector as a
+"pole".  The vectors do not need to be unit length or even perpendicular to this vector.
+But, any vectors co-linear with this vector are discarded.
+
+The first vector given (which is not co-linear with this vector) will be considered the
+0-degree heading, and so also will be returned first.
+
+=cut
+
+sub sort_vectors_by_heading {
+	my $self= shift;
+	# Cross this vector with the first vector not co-linear with this one,
+	# to create a "90 degree" vector.  Co-linear vectors are returned first.
+	my @to_sort;
+	my @to_sort_cross;
+	for (@_) {
+		my $vec90= $self->cross($_)->normalize;
+		if ($vec90->[0] || $vec90->[1] || $vec90->[2]) {
+			push @to_sort, $_;
+			push @to_sort_cross, $vec90;
+		}
+	}
+	# If there are two or fewer vectors, then they are automatically "sorted".
+	return @to_sort unless @to_sort > 2;
+	# consider first vector to be 0-degrees, and thus first cross is 90 degrees.
+	# $self X vec90 gives a 180 vec, or 90 degree vec with respect to the other cross products.
+	my $first= shift @to_sort;
+	my $vec0= shift @to_sort_cross;
+	my $vec90= $self->cross($vec0)->normalize;
+	# vec0 and vec90 and all to_sort_cross are normal vectors now, so no more need to normalize
+	my @dot0= map $_->dot($vec0), @to_sort_cross;
+	my @dot90= map $_->dot($vec90), @to_sort_cross;
+	return $first, map $to_sort[$_], sort {
+			($dot90[$a] >= 0? 1 - $dot0[$a] : 3 + $dot0[$a])
+			<=>
+			($dot90[$b] >= 0? 1 - $dot0[$b] : 3 + $dot0[$b])
+		} 0..$#to_sort;
 }
 
 sub set_projection_origin {
